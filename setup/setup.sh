9e28e5f8400f1d4b342528ab58c5d23d5c86e183
Hyuns <46562466+HyunsDev@users.noreply.github.com>
2021-07-09 11:13:59 +0900

    edit

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,7 +3,4 @@
 cd ~
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y fonts-unfonts-core
-git clone https://github.com/HyunsDev/minecraft-with-python-seoryeong-highschool
-cd minecraft-with-python-seoryeong-highschool
 ECHO "Install Finish"
