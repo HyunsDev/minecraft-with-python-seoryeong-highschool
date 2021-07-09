896772a3daf3ea5d66282b99b20e26f7a90e0ebb
Hyuns <46562466+HyunsDev@users.noreply.github.com>
2021-07-09 11:07:43 +0900

    code edit

--- 02_player_teleport.py
+++ 02_player_teleport.py
@@ -3,8 +3,8 @@ from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
 pos = mc.player.getTilePos() # 플레이어의 좌표 가져오기
-pos.x -= 5 # x축으로 -5칸한 좌표
-pos.y += 3 # y축으로 +3칸한 좌표
-pos.z -= 5 # z축으로 -5칸한 좌표
+pos.x = pos.x - 5 # x축으로 -5칸한 좌표
+pos.y = pos.y + 3 # y축으로 +3칸한 좌표
+pos.z = pos.z - 5 # z축으로 -5칸한 좌표
 
 mc.player.setTilePos(pos.x, pos.y, pos.z) # 플레이어의 좌표를 위에서 구한 좌표로 설정(tp)
